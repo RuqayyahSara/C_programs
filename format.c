37478c731b3d29d56c67ad7faa690104b80bf085
RuqayyahSara <sarasadiya4@gmail.com>
2021-10-09 23:56:47 +0530

    Print string or chars using octal or hex numbers

---
+++ format.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n1,n2;
+    printf("Enter Number1: ");
+    scanf("%2d",&n1);
+    printf("ENter Number2: ");
+    scanf("%5d",&n2);
+    printf("Number1: %d \nNumber2: %d\n",n1,n2);
+
+}  
+
+
+
+
+
