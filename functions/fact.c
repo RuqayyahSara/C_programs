e875e6ad0edb14f64869181aca57903d99ee8624
RuqayyahSara <sarasadiya4@gmail.com>
2021-11-01 06:29:41 +0530

    Factorial using iterative approach in functions

---
+++ functions/fact.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+// declaration
+char *numberType(int x);
+int fact(int x);
+
+//definition - numberType
+char *numberType(int x)
+{
+    if (x == 0)
+        return "Neither positive nor negative";
+
+    else if (x > 0)
+        return "Positive Number";
+
+    else
+        return "Negative Number";
+}
+
+//definition - Factorial
+int fact(int x)
+{
+    if (x <= 0)
+        return 0;
+
+    else
+    {
+        int f = 1;
+        for (int i = 1; i <= x; i++)
+        {
+            f = f * i;
+        }
+        return f;
+    }
+}
+
+void main()
+{
+    int x, f;
+    printf("Enter x: ");
+    scanf("%d", &x);
+    f = fact(x);
+    printf("%d is %s\n", x, numberType(x));
+    printf("Factorial of %d = %d\n", x, f);
+}
